12d436ca57f83f9f447d031b717e4c78e2e81859
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-11-02 12:52:59 +0100

    fix module

--- flake.nix
+++ flake.nix
@@ -1,18 +1,58 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
-      flake-utils,
       nixpkgs,
+      flake-utils,
       naersk,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
+    {
+      homeModules = rec {
+        atuin-fish-sync-rs =
+          {
+            lib,
+            config,
+            pkgs,
+            ...
+          }:
+          let
+            sync = self.packages.${pkgs.system}.atuin-fish-sync-rs;
+          in
+          {
+            options.programs.atuin-fish-sync-rs.enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "Enable automatic Fish history sync with atuin-fish-sync-rs";
+            };
+
+            config = lib.mkIf config.programs.atuin-fish-sync-rs.enable {
+              home.packages = [ sync ];
+
+              programs.fish.interactiveShellInit = ''
+                ${sync}/bin/atuin-fish-sync-rs &>/dev/null &
+
+                function _sync_atuin_fish --on-event fish_postexec
+                    if not set -q fish_private_mode
+                        ${sync}/bin/atuin-fish-sync-rs &>/dev/null &
+                    end
+                end
+              '';
+            };
+          };
+        default = atuin-fish-sync-rs;
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = (import nixpkgs) {
@@ -28,49 +68,18 @@
       in
       {
         packages = {
-          atuin-fish-sync-rs = atuin-fish-sync-rs;
+          inherit atuin-fish-sync-rs;
           default = atuin-fish-sync-rs;
         };
 
-        homeModules.atuin-fish-sync-rs =
-          { lib, config, ... }:
-          {
-            options.programs.atuin-fish-sync-rs.enable = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-              description = "Enable automatic Fish history sync with atuin-fish-sync-rs";
-            };
-
-            config =
-              let
-                sync = "${atuin-fish-sync-rs}/bin/atuin-fish-sync-rs";
-              in
-              lib.mkIf config.programs.atuin-fish-sync-rs.enable {
-                home.packages = [ sync ];
-
-                programs.fish.interactiveShellInit = ''
-                  ${sync} &>/dev/null &
-                '';
-
-                programs.fish.initExtra = ''
-                  function _sync_atuin_fish --on-event fish_postexec
-                      if not set -q fish_private_mode
-                          ${sync} &>/dev/null &
-                      end
-                  end
-                '';
-              };
-          };
-
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            rustc
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rustc
           ];
-
-          packages = with pkgs; [
-            rust-analyzer
-            sqlx-cli
+          packages = [
+            pkgs.rust-analyzer
+            pkgs.sqlx-cli
           ];
         };
       }
