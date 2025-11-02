d697c59674f3f591681886ac262504fbdc404ade
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-11-02 12:21:22 +0100

    package with nix

--- flake.nix
+++ flake.nix
@@ -21,16 +21,55 @@
 
         naersk-lib = pkgs.callPackage naersk { };
 
-        atuin-fish-sync = naersk-lib.buildPackage {
-          pname = "atuin-fish-sync";
+        atuin-fish-sync-rs = naersk-lib.buildPackage {
+          pname = "atuin-fish-sync-rs";
           src = ./.;
         };
       in
       {
         packages = {
-          inherit atuin-fish-sync;
-          default = atuin-fish-sync;
+          atuin-fish-sync = atuin-fish-sync-rs;
+          default = atuin-fish-sync-rs;
         };
+
+        nixosModules.atuin-fish-sync-rs =
+          { lib, ... }:
+          {
+            options.programs.atuin-fish-sync-rs.enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "Enable automatic Fish history sync with atuin-fish-sync-rs";
+            };
+
+            config =
+              {
+                config,
+                lib,
+                ...
+              }:
+              let
+                sync = "${atuin-fish-sync-rs}/bin/atuin-fish-sync";
+              in
+              {
+                options = { };
+                config = lib.mkIf config.programs.atuin-fish-sync-rs.enable {
+                  environment.systemPackages = [ sync ];
+
+                  programs.fish.interactiveShellInit = ''
+                    # Run atuin-fish-sync-rs once per shell startup
+                    ${sync} &>/dev/null &
+                  '';
+
+                  programs.fish.initExtra = ''
+                    function _sync_atuin_fish --on-event fish_postexec
+                        if not set -q fish_private_mode
+                            ${sync} &>/dev/null &
+                        end
+                    end
+                  '';
+                };
+              };
+          };
 
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
